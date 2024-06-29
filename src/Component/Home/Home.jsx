40c51d22eae530cadb54f8ca104593a9e9c95ee3
unknown <showlinakter77@gmail.com>
2024-06-30 01:40:27 +0600

    create a login page

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
 
 
 const Home = () => {
     return (
         <div className="mt-8">
             <Banner></Banner>
+            <div className="mt-16">
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
