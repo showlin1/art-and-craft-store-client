a7139c94743e269d2c4b0d19e990236d2033958d
unknown <showlinakter77@gmail.com>
2024-07-02 17:16:55 +0600

    404Page complete

--- src/Component/Routes/Routes.jsx
+++ src/Component/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import Root from "../Layout/Root";
 import Home from "../Home/Home";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
+import Page404 from "../NotFound/Page404";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement:<Page404></Page404>,
       children:[
         {
             path:'/',
