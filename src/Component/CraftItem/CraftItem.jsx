2bc0d330cb5511350f32f27d85523ff30951b6d3
unknown <showlinakter77@gmail.com>
2024-07-03 12:53:32 +0600

    craft Item section complete

--- src/Component/CraftItem/CraftItem.jsx
+++ src/Component/CraftItem/CraftItem.jsx
@@ -1,9 +1,38 @@
+import { Link } from "react-router-dom";
 
 
-const CraftItem = () => {
+const CraftItem = ({ artAndCraft }) => {
+
+    const { id, imageUrl, itemName, subcategoryName, description, price, rating, customization, processingTime, stockStatus, userEmail, userName } = artAndCraft;
+
     return (
         <div>
-            
+            <div className="p-6  border-2 h-[1000px] card bg-base-100 w-96 shadow-xl">
+                <figure className="  rounded-xl">
+                    <img
+                        src={imageUrl}
+                        className="rounded-xl w-[350px] h-[300px]" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title text-center font-bold text-2xl mb-2 ">{itemName}</h2>
+                    <p className="mb-2 text-xl"><span className="font-bold">Subcategory Name: </span> {subcategoryName}</p>
+                    <p className="mb-2 text-xl"><span className="font-bold">Description: </span>{description}</p>
+                    <div className="flex justify-end">
+                        <p className="mb-2 text-xl"><span className="font-bold">price: </span> {price}</p>
+                        <p className="mb-2 text-xl"><span className="font-bold">Rating: </span> {rating}</p>
+                    </div>
+                    <p className="mb-2 text-xl"><span className="font-bold">Customization: </span> {customization}</p>
+                    <p className="mb-2 text-xl"><span className="font-bold">Stock Status: </span> {stockStatus}</p>
+                    <p className="mb-2 text-xl"><span className="font-bold">Processing Time: </span>{processingTime}</p>
+
+                    <div>
+                        <Link to={`/artAndCraft/${id}`}>
+                            <button className="btn btn-primary w-full">View Details</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
+import CraftItem from "../CraftItem/CraftItem";
 
 
 const Home = () => {
@@ -10,6 +11,16 @@ const Home = () => {
             <Banner></Banner>
             <div className="mt-14 mb-6">
                 <h1 className="text-6xl text-center font-bold text-purple-600">Painting and Drawing:{artAndCrafts.length} </h1>
+
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-14 mb-20">
+                {
+                    artAndCrafts.map(artAndCraft => <CraftItem
+                        key={artAndCraft._id}
+                        artAndCraft={artAndCraft}
+                    ></CraftItem>)
+                }
+                </div>
+               
             </div>
             <div className="mt-16">
                 <Footer></Footer>
