78290ef779b2b69993b7e81b8e77071c1b65c0d7
unknown <showlinakter77@gmail.com>
2024-07-04 22:08:59 +0600

    firebase config file updated

--- src/Component/Firebase/firebase.config.js
+++ src/Component/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZX0duscqy82aI-DFJ1GukzwG-4wRE_Ds",
-  authDomain: "art-and-craft-store-f0b37.firebaseapp.com",
-  projectId: "art-and-craft-store-f0b37",
-  storageBucket: "art-and-craft-store-f0b37.appspot.com",
-  messagingSenderId: "849426295841",
-  appId: "1:849426295841:web:3d4695f2ea1b59c89b0760"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
